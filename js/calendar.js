7cc5980df645536ae55b2a8eda1ccae283c66f6a
Matt Ketkaroonkul <matt-ketk@github.com>
2020-12-26 20:22:35 -0800

    was a nice experiement, but no

--- js/calendar.js
+++ js/calendar.js
@@ -49,6 +49,49 @@ function handleSignoutClick(event) {
     gapi.auth2.getAuthInstance().signOut();
 }
 
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
 function listUpcomingEvents() {
-    console.log('signed in');
+    gapi.client.calendar.events.list({
+      'calendarId': 'primary',
+      'timeMin': (new Date()).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'maxResults': 8,
+      'orderBy': 'startTime'
+    }).then(function(response) {
+      var events = response.result.items;
+      var entries = document.getElementsByClassName('qagenda__entry');
+      if (events.length > 0) {
+        for (i = 0; i < events.length; i++) {
+          var event = events[i];
+            console.log(event);
+          var whenStart = event.start.dateTime;
+          var whenEnd = event.end.dateTime;
+          var when = "";
+          if (!whenStart) {
+            when = event.start.date;
+          } else {
+            var dStart = new Date(event.start.dateTime);
+            var hStart = addZero(dStart.getHours());
+            var mStart = addZero(dStart.getMinutes());
+            var dEnd = new Date(event.end.dateTime);
+            var hEnd = addZero(dEnd.getHours());
+            var mEnd = addZero(dEnd.getMinutes());
+            when = hStart + ":" + mStart + "-" + hEnd + ":" + mEnd;
+          }
+          entries[i].innerHTML = event.summary + ": " + when;
+          entries[i].style.display = "center";
+        console.log(i);
+        }
+      } else {
+          entries[0].innerHTML = "No upcoming events!";
+          entries[0].style.display = "center";
+      }
+    });
 }
