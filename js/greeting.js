51b1a8e5ed856f1e33c8ef7b1e3e473b173cb6ae
Matt Ketkaroonkul <matt-ketk@github.com>
2020-12-27 22:26:29 -0800

    tweaked reminder

--- js/greeting.js
+++ js/greeting.js
@@ -13,17 +13,13 @@ var gree4 = 'Good evening, ';
 var gree5 = 'Good evening ';
 var gree6 = 'Good evening ';
 
-// Reminder to check calendar
-var reminder = (diff>=REMIND_TIME*1000*3600) ? 
-    "!\nRemember to check your calendar!" : "!";
-
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-    document.getElementById('greetings').innerText = gree1 + name + reminder;
+    document.getElementById('greetings').innerText = gree1 + name + "!";
 } else if (hour >= 6 && hour < 12) {
-    document.getElementById('greetings').innerText = gree2 + name + reminder;
+    document.getElementById('greetings').innerText = gree2 + name + "!";
 } else if (hour >= 12 && hour < 17) {
-    document.getElementById('greetings').innerText = gree3 + name + reminder;
+    document.getElementById('greetings').innerText = gree3 + name + "!";
 } else  {
-    document.getElementById('greetings').innerText = gree4 + name + reminder;
+    document.getElementById('greetings').innerText = gree4 + name + "!";
 }
--- js/reminder.js
+++ js/reminder.js
@@ -1,35 +1,40 @@
-const REMIND_TIME = 3; // in hours
+const REMIND_TIME = 3 * 1000 * 3600; // in ms 
+const INTERVAL = 1000;
+const reminderText = "\nRemember to check your calendar!";
 
 var calendarButton = document.getElementById('calendar');
+var lastClicked = Date.now();
+var remindActive = false;
 var blinkOn = false;
-var diff = -1;
 
-function blink() {
-    if (!blinkOn) {
-        calendarButton.classList.add('qlink__link-3-active');
-        blinkOn = true;
-    } else {
-        calendarButton.classList.remove('qlink__link-3-active');
-        blinkOn = false;
+function remindTick() {
+    if (!remindActive) {
+        if (Date.now() - lastClicked >= REMIND_TIME) {
+            setTimeout(function() {
+                document.getElementById('greetings').innerText += reminderText;
+            }, 50);
+            remindActive = true;
+        }
     }
-    setTimeout(blink, 1000);
+    if (remindActive) {
+        if (!blinkOn) {
+            calendarButton.classList.add('qlink__link-3-active');
+            blinkOn = true;
+        } else {
+            calendarButton.classList.remove('qlink__link-3-active');
+            blinkOn = false;
+        }
+    }
+    setTimeout(remindTick, INTERVAL);
 }
 
-function saveTimestamp() {
+calendarButton.onclick = function() {
     localStorage.setItem('lastClicked', Date.now());
     console.log(localStorage.getItem('lastClicked'));
 }
 
-calendarButton.onclick = saveTimestamp;
 if (!(localStorage.getItem('lastClicked') === null)) {
     var lastClicked = localStorage.getItem('lastClicked'); 
-    diff = Date.now() - lastClicked;
-    console.log(diff);
-
-}
-if (diff >= REMIND_TIME * 1000 * 3600) {
-    blink();
 }
 
-
-
+remindTick();
